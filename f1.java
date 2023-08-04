b35d893d97d40d98d551edcb4396f0bf47ec840e
user.email <samiuddin.adse@gmail.com>
2023-08-04 11:11:49 +0530

    java file added
    java file added,html added

---
+++ f1.java
@@ -0,0 +1,32 @@
+class Main {
+
+  
+ public  Main(){
+}
+public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello World!</title>
+<link rel="stylesheet" href="bluestyle.css">
+</head>
+<body>
+
+<h1>Hello world!</h1>
+<div><img src="img_hello_world.jpg" alt="Hello World from Space"
+style="width:100%;max-width:960px"></div>
+<p>This is the first file in my new Git Repo.</p>
+
+<div> <p> this is the block added just now </p></div>
+
+</body>
+</html>
